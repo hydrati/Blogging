1f1ac3f5c987894f5b59809fa332c4a50196be18
186526 <186526@186526.club>
2020-07-25 23:16:59 +0800

    Fix bug when path is not "/"

--- post/About.md
+++ post/About.md
@@ -1,4 +1,5 @@
 <div align="center">
+    <h1>About me</h1>
     <a href="https://github.com/186526"><img src='https://github-readme-stats.vercel.app/api?username=186526&hide_border=true' alt='github stats' /></a>
     <h3>A developer who develop with love & bug</h3>
 </div>
--- theme/classic/src/intro.js
+++ theme/classic/src/intro.js
@@ -1,36 +1,66 @@
-document.getElementById('avatar').src = config.theme_config.avatar;
-document.getElementById('intro-text-title').innerHTML = config.theme_config.author + "'s Blog";
-document.getElementById('intro-text-intro').innerHTML = "—— " + config.theme_config.introduction;
+document.getElementById("avatar").src = config.theme_config.avatar;
+document.getElementById("intro-text-title").innerHTML =
+  config.theme_config.author + "'s Blog";
+document.getElementById("intro-text-intro").innerHTML =
+  "—— " + config.theme_config.introduction;
 
 async function init_post_container() {
-    let post_container = "";
-    for(let x=0;x<config.post.length;x++){
-        const post = {};
-        post.url = settings.post+config.post[x].url+".md";
-        await fetch(post.url)
-        .then(response => response.text())
-        .then(content => {
-            post.preview = {
-                response: content,
-            };
-            post.preview.mdcontent = marked(content);
-            return marked(content);
-        })
-        .then(content => {
-            document.getElementsByClassName("none")[0].innerHTML = content;
-        }).then(function(){
-            post.preview.title=document.getElementsByClassName("none")[0].getElementsByTagName("h1")[0].innerText;
-            post.preview.intro=config.post[x].introduction;
-        })
-        .then(function(){
-            post.container = '<div class="post-container"><h1>'+post.preview.title+'</h1><p class="post-intro">'+post.preview.intro+'</p><p><a href="/?p='+config.post[x].url+'">Reading<i class="material-icons arrow_forward"></i></a></p></div>';
-            post_container = post_container+post.container;
-        });
-    }
-    return post_container;
+  let post_container = "";
+  for (let x = 0; x < config.post.length; x++) {
+    const post = {};
+    post.url = settings.post + config.post[x].url + ".md";
+    await fetch(post.url)
+      .then((response) => response.text())
+      .then((content) => {
+        post.preview = {
+          response: content,
+        };
+        post.preview.mdcontent = marked(content);
+        return marked(content);
+      })
+      .then((content) => {
+        document.getElementsByClassName("none")[0].innerHTML = content;
+      })
+      .then(function () {
+        post.preview.title = document
+          .getElementsByClassName("none")[0]
+          .getElementsByTagName("h1")[0].innerText;
+        post.preview.intro = config.post[x].introduction;
+      })
+      .then(function () {
+        if (window.location.search == "?page=archive") {
+          post.container =
+            '<div class="post-container"><h1>' +
+            post.preview.title +
+            '</h1><div class="post-info">' +
+            post.preview.mdcontent +
+            '</div><p><a href="?p=' +
+            config.post[x].url +
+            '">Reading<i class="material-icons arrow_forward"></i></a></p></div>';
+        } else {
+          post.container =
+            '<div class="post-container"><h1>' +
+            post.preview.title +
+            '</h1><p class="post-intro">' +
+            post.preview.intro +
+            '</p><p><a href="?p=' +
+            config.post[x].url +
+            '">Reading<i class="material-icons arrow_forward"></i></a></p></div>';
+        }
+        post_container = post_container + post.container;
+      });
+  }
+  return post_container;
 }
-if (window.location.search==="?page=home"){
-    init_post_container().then(content => {
-        document.getElementById("post").innerHTML = content;
-    });
+add_pic = function () {
+  var imgs = document.getElementsByTagName("img");
+  for (var i = 0; i < imgs.length; i++) {
+    imgs[i].loading = "lazy";
+  }
+};
+if (getpar("page")) {
+  init_post_container().then((content) => {
+    document.getElementById("post").innerHTML = content;
+  });
 }
+add_pic();
